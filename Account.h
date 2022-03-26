3a0b1d599b93111f7d784965cf4f89214d9e99b7
Walsh-Daniel <Danielwalsh682@gmail.com>
2022-03-26 11:36:02 -0700

    Dan / Zak PA3

--- Account.h
+++ Account.h
@@ -10,16 +10,20 @@ class Account{
     private:
         //instance of money class to track balance
         Money balance;
+
         //vectors for deposits and withdrawals
         std::vector<Money> deposits;
         std::vector<Money> withdrawals;
-        //to track if balance is up to date and the number of deposits/withdrawals made
+
+        //to track if balance is up-to-date and the number of deposits/withdrawals made
         bool upToDate = true;
         int numDeposits = 0, numWithdrawals = 0;
 
     public:
+
         //parameterized constructor with an instance of money class passed in
         Account(Money);
+
         //methods to make deposits/withdrawals
         void makeDeposit(Money);
         void makeWithdrawals(Money);
